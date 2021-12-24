29d055a776d4cbb010877d9ddbfdf26443855d4a
Manthan <manthanmevada66@gmail.com>
2021-12-24 19:47:19 +0530

    Cleaned Up Code and Added Square Images Upload from any format to sticker
    Can upload now squared off images with min 512x512 res

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:whatsapp_stickers/exceptions.dart';
 import 'package:whatsapp_stickers/whatsapp_stickers.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 void main() {
   runApp(AppRoot());
@@ -23,8 +26,8 @@ class AppRoot extends StatelessWidget {
               Center(
                 child: Padding(
                   padding: const EdgeInsets.only(top: 32.0),
-                  child: RaisedButton(
-                    child: Text('Pick 3 webp files'),
+                  child: ElevatedButton(
+                    child: Text('Pick 3 Image files'),
                     onPressed: () async {
                       FilePickerResult result = await FilePicker.platform
                           .pickFiles(allowMultiple: true);
@@ -52,7 +55,7 @@ class AppRoot extends StatelessWidget {
 
 Future installFromFileList(List<File> fileList) async {
   var stickerPack = WhatsappStickers(
-    identifier: 'cuppyFlutterWhatsAppStickers',
+    identifier: 'TestDemo6666',
     name: 'Cuppy Flutter WhatsApp Stickers',
     publisher: 'John Doe',
     trayImageFileName: WhatsappStickerImage.fromAsset('assets/tray_Cuppy.png'),
@@ -61,14 +64,37 @@ Future installFromFileList(List<File> fileList) async {
     licenseAgreementWebsite: '',
   );
 
-  fileList.forEach((file) {
-    stickerPack.addSticker(
-        WhatsappStickerImage.fromFile('${file.path}'), ['🙂', '😛']);
+  addImagesToWhatsapp(fileList, stickerPack).then((_) async {
+    try {
+      await stickerPack.sendToWhatsApp();
+    } on WhatsappStickersException catch (e) {
+      print(e.cause);
+    }
   });
+}
 
-  try {
-    await stickerPack.sendToWhatsApp();
-  } on WhatsappStickersException catch (e) {
-    print(e.cause);
-  }
+convertFileToWebp(File file) async {
+  final directory = await getApplicationDocumentsDirectory();
+  final imageFile = file.path.split('/').last.split('.').first;
+  final imageExtension = file.path.split('/').last.split('.').last;
+  Uint8List imageData = await file.readAsBytes();
+  imageData = Uint8List.fromList(imageData);
+  print('imgData is $imageData');
+  final result = await FlutterImageCompress.compressWithList(imageData,
+      minHeight: 512, minWidth: 512, quality: 90, format: CompressFormat.webp);
+  print('result is $result');
+  final tempPath = await File('${directory.path}/$imageFile.webp').create();
+  print('tempPath is $tempPath');
+  File resultFile = await tempPath.writeAsBytes(result);
+  return resultFile;
+}
+
+Future addImagesToWhatsapp(fileList, stickerPack) {
+  return Future.forEach(fileList, (file) async {
+    File imgFile = await convertFileToWebp(file);
+    WhatsappStickerImage stickerImage =
+        WhatsappStickerImage.fromFile(imgFile.path);
+    const emojis = ['🙂', '😛'];
+    stickerPack.addSticker(stickerImage, emojis);
+  });
 }
